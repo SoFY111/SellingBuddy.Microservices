05b23a262ee862e5f5cbd2465d710157f830ae58
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2022-02-20 17:18:07 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBusBase/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EventBusBase
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -0,0 +1,43 @@
+using CatalogService.Api.Infrastructure.Context;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using CatalogService.Api.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+            hostBuilder.MigrateDbContext<CatalogContext>((context, services) =>
+            {
+                var env = services.GetService<IWebHostEnvironment>();
+                var logger = services.GetService<ILogger<CatalogContextSeed>>();
+
+                new CatalogContextSeed()
+                    .SeedAsync(context, env, logger)
+                    .Wait();
+            });
+
+            hostBuilder.Run();
+        }
+
+        static IWebHost CreateHostBuilder(string[] args) {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseWebRoot("Pics")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build();
+        }
+    }
+}
